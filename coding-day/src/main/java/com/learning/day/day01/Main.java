c995d21ab00aa03a86c08b12b9505088f622f74f
glongone <glongong@outlook.com>
2020-04-02 21:23:01 +0800

    初始化工程

---
+++ coding-day/src/main/java/com/learning/day/day01/Main.java
@@ -0,0 +1,13 @@
+package com.learning.day.day01;
+
+/**
+ * @author glong
+ * @date 2020/4/2 21:20
+ *
+ * 开始学习
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("start learning everyday");
+    }
+}
