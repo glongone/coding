4d1acaa769302ee9cd137a973eb987452893a774
glongone <Benlong.Gao@geely.com>
2020-08-24 13:40:48 +0800

    dcl

--- coding-design/src/main/java/com/learn/coding/singleton/Singleton2.java
+++ coding-design/src/main/java/com/learn/coding/singleton/Singleton2.java
@@ -1,11 +1,11 @@
 package com.learn.coding.singleton;
 
 /**
- * 单例模式：双检锁/双重校验锁
+ * 单例模式：双检锁/双重校验锁 DCL
  * 是否 Lazy 初始化：是
  * 是否多线程安全：是
  * 这种方式采用双锁机制，安全且在多线程情况下能保持高性能。
- * */
+ */
 
 public class Singleton2 extends Singleton{
     private volatile static Singleton2 singleton;
